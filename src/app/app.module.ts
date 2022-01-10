1b76ac06fe58a6b59c55bc09bec114f9a26f8336
Jitendrad Prajapati <jitendradprajapati@yahoo.com>
2022-01-10 23:23:47 +0530

    Created direcive to display dummy image if image not found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HandleImageErrorDirective } from './directives/handle-image-error.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HandleImageErrorDirective
   ],
   imports: [
     BrowserModule,
