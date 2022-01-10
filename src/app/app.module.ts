855beb5d865fa7d7a888e40ade3e6b83e5d10984
Jitendrad Prajapati <jitendradprajapati@yahoo.com>
2022-01-11 00:03:15 +0530

    created directive to scoll to particular div

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HandleImageErrorDirective } from './directives/handle-image-error.directive';
+import { ScrollToElementDirective } from './directives/scroll-to-element.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HandleImageErrorDirective
+    HandleImageErrorDirective,
+    ScrollToElementDirective
   ],
   imports: [
     BrowserModule,
